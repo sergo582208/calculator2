a5e14b1c28b16fd4bf83fd092f30c175b483d5b7
hp <747685@mail.ru>
2023-04-17 20:50:06 +0300

    домашнее задание

---
+++ src/main/java/calculator/CalculatorServiceImpl.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import calculator.service.CalculatorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public String plus(Integer num1, Integer num2) {
+        return String.valueOf(num1 + num2);
+    }
+
+    @Override
+    public String minus(Integer num1, Integer num2) {
+        return String.valueOf(num1 - num2);
+    }
+
+    @Override
+    public String multiply(Integer num1, Integer num2) {
+        return String.valueOf(num1 * num2);
+    }
+
+    @Override
+    public String divide(Integer num1, Integer num2) {
+        return String.valueOf(num1/num2);
+    }
+}
---
+++ src/main/java/calculator/calculatorspringdemo/CalculatorController.java
@@ -0,0 +1,52 @@
+package calculator.calculatorspringdemo;
+
+import calculator.service.CalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    public CalculatorController() {
+        this(null);
+    }
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping()
+    public String welcome(){
+        return "Добро пожаловать в калькулятор";
+    }
+    @GetMapping("/plus")
+    public String plus(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2){
+        if (num1 == null || num2 == null){
+            return "Отсутствуют входные значения";
+        }
+        return calculatorService.plus(num1,num2);
+    }
+    @GetMapping("/minus")
+    public String minus(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2){
+        if (num1 == null || num2 == null) {
+            return "Отсутствуют входные значения";
+        }
+        return calculatorService.minus(num1,num2);
+    } @GetMapping("/multiply")
+    public String multiply(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
+        if (num1 == null || num2 == null) {
+            return "Отсутствуют входные значения";
+        }
+        return calculatorService.multiply(num1,num2);
+    } @GetMapping("/divide")
+        public String divide(@RequestParam(name = "num1", required = false) Integer num1, @RequestParam(name = "num2", required = false) Integer num2) {
+        if (num2 == 0) {
+            return "Деление запрещено";
+        }
+        return calculatorService.divide(num1,num2);
+        }
+    }
+
