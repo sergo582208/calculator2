1de0a93babda67b903f86f0efdde598f20137bce
hp <747685@mail.ru>
2023-04-17 20:55:28 +0300

    домашнее задание

--- src/main/java/calculator/calculatorspringdemo/CalculatorController.java
+++ src/main/java/calculator/calculatorspringdemo/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculator")
-public class CalculatorController {
+public class CalculatorController{
     public CalculatorController() {
         this(null);
     }
--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -5,4 +5,5 @@ public interface CalculatorService {
     String minus (Integer num1, Integer num2);
     String multiply (Integer num1, Integer num2);
     String divide (Integer num1,Integer num2);
+
 }
